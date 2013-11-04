1f3d1cdcc7c4bd43cd5f2eeeb59bac41c07f2fc8
Oleg Grishin <og402@nyu.edu>
2013-11-04 12:47:35 +0400

    Empty project

---
+++ OneForm/OFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OFAppDelegate.h
+//  OneForm
+//
+//  Created by Oleg Grishin on 11/4/13.
+//  Copyright (c) 2013 nyuad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OneForm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OneForm
+//
+//  Created by Oleg Grishin on 11/4/13.
+//  Copyright (c) 2013 nyuad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OFAppDelegate class]));
+    }
+}
