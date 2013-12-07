5cdd1539c26ee0a161d0ceb733f177d9567f26f2
Oleg Grishin <og402@nyu.edu>
2013-12-08 01:01:15 +0400

    Redesigned UITableView in search forms controller and deleted QR

--- OneForm/OFSearchFormsViewController.m
+++ OneForm/OFSearchFormsViewController.m
@@ -52,9 +52,9 @@
     
     //DUMMY
     
-    self.popularForms = @[@"UAE driver's license form", @"Naturalization form", @"Birth certificate", @"Change of address", @"Visa application form", @"Visa renewal form"];
+    self.popularForms = @[@[@"UAE driver's license form", @"Abu Dhabi Customs Administration"], @[@"Naturalization form", @"Abu Dhabi Chamber of Commerce and Industry"], @[@"Birth certificate" , @"Abu Dhabi Education Council Council Council Council"], @[@"Change of address", @"Abu Dhabi home authority"], @[@"Visa application form", @"UAE Customs"], @[@"Visa renewal form", @"Immigration"]];
     
-    self.popularFormsTable = [[UITableView alloc] initWithFrame:CGRectMake(34.75f, 262.5f, 237.75f, 260) style:UITableViewStylePlain];
+    self.popularFormsTable = [[UITableView alloc] initWithFrame:CGRectMake(34.75f, 252.5f, 237.75f, 280) style:UITableViewStylePlain];
     [self.popularFormsTable setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     [self.popularFormsTable setShowsVerticalScrollIndicator:NO];
     [self.popularFormsTable setDelegate:self];
@@ -79,29 +79,33 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 52;
+    return 100;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     NSString *cellIdentifier = [NSString stringWithFormat:@"Cell%i",indexPath.row];
     UILabel *cellLabel;
+    UILabel *organizationName;
     UIImageView *arrow;
-    float height = 52;
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
         [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
-        cellLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 207.25, height)];
+        cellLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 5, 207.25, 52)];
         [cellLabel setTextColor:UI_COLOR];
         [cellLabel setNumberOfLines:2];
         [cellLabel setFont:[UIFont fontWithName:@"Roboto-Regular" size:21]];
         arrow = [[UIImageView alloc] initWithFrame:CGRectMake(self.popularFormsTable.frame.size.width - 20.25f, 0, 20.25f, 33.75f)];
-        arrow.center = CGPointMake(arrow.center.x, height/2);
+        arrow.center = CGPointMake(arrow.center.x, 26);
         [arrow setImage:[UIImage imageNamed:@"form_choice_arrow.png"]];
         [cell.contentView addSubview:arrow];
+        organizationName = [[UILabel alloc] initWithFrame:CGRectMake(0, 56, 207.25, 38)];
+        [organizationName setTextColor:UI_COLOR];
+        [organizationName setNumberOfLines:2];
+        [organizationName setFont:[UIFont fontWithName:@"Roboto-Light" size:16]];
         
         UITapGestureRecognizer *goToForm = [[UITapGestureRecognizer alloc]
                                                        initWithTarget:self
@@ -109,10 +113,20 @@
         [goToForm setCancelsTouchesInView:NO];
         [cell.contentView setUserInteractionEnabled:YES];
         [cell.contentView  addGestureRecognizer:goToForm];
+    
+        NSArray *info = [self.popularForms objectAtIndex:indexPath.row];
+        [cellLabel setText:info[0]];
+        [cellLabel sizeToFit];
+        [organizationName setText:info[1]];
+        if ((int)(cellLabel.frame.size.height/cellLabel.font.leading) == 1)
+        {
+            [organizationName setFrame:CGRectMake(0, 40, 207.25, 20 * (int)(organizationName.frame.size.height/organizationName.font.leading))];
+            [cellLabel setFrame:CGRectMake(0, 0, 207.25, 52)];
+        }
+        [cell.contentView addSubview:organizationName];
+        [cell.contentView addSubview:cellLabel];
     }
     
-    [cellLabel setText:[self.popularForms objectAtIndex:indexPath.row]];
-    [cell.contentView addSubview:cellLabel];
     return cell;
     
 }
